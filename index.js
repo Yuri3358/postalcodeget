ffba9ade138371c26a4f6140e3a9f17a80d439ca
yuri3358 <yurialmeida917@gmail.com>
2022-08-07 19:42:19 -0300

    XHR removed and Fetch added

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-function Search(url) {
-    let xhr = new XMLHttpRequest()
-    xhr.open("GET", url, false)
-    xhr.send()
-    let output = JSON.parse(xhr.responseText)
-    return output
-}
-
-function Show() {
+async function Show() {
     let cep = document.getElementById('cep').value
     let cep_input = document.querySelector('input#cep')
     let output = document.querySelector("div#output")
-    let request = Search(`https://cep.awesomeapi.com.br/json/${cep}`)
-    let address = request['address']
-    let state = request['state']
-    let city = request['city']
-    let district = request['district']
+    
+    let request = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
+    let data = await request.json()
+    let address = data['address']
+    let state = data['state']
+    let city = data['city']
+    let district = data['district']
 
     
-    if (request['status'] == 404 || request['status'] == 400) {
+    if (data['status'] == 404 || data['status'] == 400) {
         output.style.color = `#f50505`
         output.innerHTML = '<p>CEP não encontrado ou inválido, digite novamente</p>'
     } else {
         output.style.color = `black`
         output.innerHTML = `<p>CEP: ${cep}</p> <p>Endereço: ${address}</p> <p>Cidade: ${city}</p> <p>Estado: ${state}</p> <p>Distrito/Bairro: ${district}</p>`
     }
-
     cep_input.focus()
     
 }
