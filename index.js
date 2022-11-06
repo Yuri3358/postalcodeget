9d61262279d55cf366d4dc6680e812891d4c3637
Yuri3358 <yurialmeida917@gmail.com>
2022-11-06 13:50:37 -0300

    style: better p organization and comment removed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 function App() {
     const [data, setData] = React.useState({})
     const [postalcode, setPostalCode] = React.useState("")
-    //[address_name, state, city]
+
     React.useEffect(() => {
         if (postalcode.length == 8) {
             fetch(`https://cep.awesomeapi.com.br/json/${postalcode}`)
@@ -11,6 +11,7 @@ function App() {
             .then(output => setData(output))     
         }
     }, [postalcode]) 
+    
     return (
         <section id="content">
             <h1>Localizador de CEP</h1>
@@ -20,9 +21,17 @@ function App() {
                 id="inputcep" 
                 onChange={e => setPostalCode(e.target.value)}></input>
             <div>
-                <p><span className="infos">Endereço</span>: {data.address_name}</p>
-                <p><span className="infos">Cidade</span>: {data.state}</p>
-                <p><span classname="infos">Bairro/distrito</span>: {data.district}</p>
+                <p>
+                    <span className="infos">Endereço</span>: {data.address_name}
+                </p>
+
+                <p>
+                    <span className="infos">Cidade</span>: {data.state}
+                </p>
+
+                <p>
+                    <span classname="infos">Bairro/distrito</span>: {data.district}
+                </p>
             </div>
         </section>
     )
