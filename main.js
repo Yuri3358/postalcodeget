15e18c78d8e11ba30dc5a3ac441cb580fa60fc63
Yuri3358 <yurialmeida917@gmail.com>
2022-11-06 14:15:50 -0300

    BREAKING CHANGE: index.js is now main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+function App() {
+    const [data, setData] = React.useState({})
+    const [postalcode, setPostalCode] = React.useState("")
+
+    React.useEffect(() => {
+        if (postalcode.length == 8) {
+            fetch(`https://cep.awesomeapi.com.br/json/${postalcode}`)
+            .then(response => response.json())
+            .then(output => setData(output))     
+        }
+    }, [postalcode]) 
+
+    return (
+        <section id="content">
+            <h1>Localizador de CEP</h1>
+            <label htmlFor="inputcep">CEP</label>
+            <input 
+                type="text" 
+                id="inputcep" 
+                onChange={e => setPostalCode(e.target.value)}></input>
+            <div id="output">
+                <p>
+                    <span className="infos">Endereço</span>: {data.address_name}
+                </p>
+
+                <p>
+                    <span className="infos">Cidade</span>: {data.city}
+                </p>
+
+                <p>
+                    <span className="infos">Bairro/distrito</span>: {data.district}
+                </p>
+            </div>
+        </section>
+    )
+}
+
+
+root.render(<App/>)
